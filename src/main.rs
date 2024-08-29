41b381daf47a31f94822ccd8ef4b39afa35a4ef9
Timothy <yexiaozhou2003@gmail.com>
2024-08-30 00:25:27 +0800

    parse the number of pings

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .index(2)
                 .help("Port to ping"),
         )
+        .arg(
+            Arg::new("n")
+                .short('n')
+                .long("num")
+                .default_value("10")
+                .help("Number of pings to send"),
+        )
         .get_matches();
 
     let host = matches.get_one::<String>("host").unwrap().clone();
     let port = matches.get_one::<String>("port").unwrap().to_owned();
     let port_num = port.parse::<u16>()?;
+    let num = matches.get_one::<String>("n").unwrap().to_owned();
+    let num_pings = num.parse::<u32>().unwrap().to_owned();
     let timeout = Duration::from_secs(5);
-    let num_pings = 7;
 
     println!(
         "TCPinging {} on port {}.",
