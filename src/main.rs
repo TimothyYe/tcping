e8b2eafb0cbe8b14b04bc4d18d4a21f0da4834b3
Timothy <yexiaozhou2003@gmail.com>
2025-09-02 21:55:21 +0800

    check the input timeout

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let num_pings = matches.get_one::<String>("n").unwrap().parse::<u32>()?;
 
     let timeout_secs = matches.get_one::<String>("t").unwrap().parse::<u64>()?;
+    if timeout_secs == 0 {
+        return Err("Timeout must be greater than 0 seconds".into());
+    }
     let timeout = Duration::from_secs(timeout_secs);
 
     let interval_ms = matches.get_one::<String>("i").unwrap().parse::<u64>()?;
@@ -100,7 +103,7 @@ mod tests {
         let matches = create_test_app()
             .try_get_matches_from(vec!["tcping", "google.com", "443"])
             .unwrap();
-        
+
         let port_result = matches.get_one::<String>("port").unwrap().parse::<u16>();
         assert!(port_result.is_ok());
         assert_eq!(port_result.unwrap(), 443);
@@ -111,7 +114,7 @@ mod tests {
         let matches = create_test_app()
             .try_get_matches_from(vec!["tcping", "google.com", "70000"])
             .unwrap();
-        
+
         let port_result = matches.get_one::<String>("port").unwrap().parse::<u16>();
         assert!(port_result.is_err());
     }
@@ -121,7 +124,7 @@ mod tests {
         let matches = create_test_app()
             .try_get_matches_from(vec!["tcping", "google.com", "443"])
             .unwrap();
-        
+
         assert_eq!(matches.get_one::<String>("n").unwrap(), "10");
         assert_eq!(matches.get_one::<String>("t").unwrap(), "3");
         assert_eq!(matches.get_one::<String>("i").unwrap(), "500");
@@ -130,9 +133,19 @@ mod tests {
     #[test]
     fn test_custom_values() {
         let matches = create_test_app()
-            .try_get_matches_from(vec!["tcping", "google.com", "443", "-n", "5", "-t", "10", "-i", "1000"])
+            .try_get_matches_from(vec![
+                "tcping",
+                "google.com",
+                "443",
+                "-n",
+                "5",
+                "-t",
+                "10",
+                "-i",
+                "1000",
+            ])
             .unwrap();
-        
+
         assert_eq!(matches.get_one::<String>("n").unwrap(), "5");
         assert_eq!(matches.get_one::<String>("t").unwrap(), "10");
         assert_eq!(matches.get_one::<String>("i").unwrap(), "1000");
@@ -141,9 +154,19 @@ mod tests {
     #[test]
     fn test_long_argument_names() {
         let matches = create_test_app()
-            .try_get_matches_from(vec!["tcping", "google.com", "443", "--num", "20", "--timeout", "5", "--interval", "200"])
+            .try_get_matches_from(vec![
+                "tcping",
+                "google.com",
+                "443",
+                "--num",
+                "20",
+                "--timeout",
+                "5",
+                "--interval",
+                "200",
+            ])
             .unwrap();
-        
+
         assert_eq!(matches.get_one::<String>("n").unwrap(), "20");
         assert_eq!(matches.get_one::<String>("t").unwrap(), "5");
         assert_eq!(matches.get_one::<String>("i").unwrap(), "200");
@@ -152,18 +175,28 @@ mod tests {
     #[test]
     fn test_parse_numeric_values() {
         let matches = create_test_app()
-            .try_get_matches_from(vec!["tcping", "localhost", "22", "-n", "15", "-t", "2", "-i", "750"])
+            .try_get_matches_from(vec![
+                "tcping",
+                "localhost",
+                "22",
+                "-n",
+                "15",
+                "-t",
+                "2",
+                "-i",
+                "750",
+            ])
             .unwrap();
-        
+
         // Test that all numeric parsing works
         let num_pings = matches.get_one::<String>("n").unwrap().parse::<u32>();
         let timeout = matches.get_one::<String>("t").unwrap().parse::<u64>();
         let interval = matches.get_one::<String>("i").unwrap().parse::<u64>();
-        
+
         assert!(num_pings.is_ok());
         assert!(timeout.is_ok());
         assert!(interval.is_ok());
-        
+
         assert_eq!(num_pings.unwrap(), 15);
         assert_eq!(timeout.unwrap(), 2);
         assert_eq!(interval.unwrap(), 750);
@@ -172,14 +205,36 @@ mod tests {
     #[test]
     fn test_zero_values() {
         let matches = create_test_app()
-            .try_get_matches_from(vec!["tcping", "localhost", "22", "-n", "0", "-t", "0", "-i", "0"])
+            .try_get_matches_from(vec![
+                "tcping",
+                "localhost",
+                "22",
+                "-n",
+                "0",
+                "-t",
+                "0",
+                "-i",
+                "0",
+            ])
             .unwrap();
-        
+
         // Test parsing zero values
-        let num_pings = matches.get_one::<String>("n").unwrap().parse::<u32>().unwrap();
-        let timeout = matches.get_one::<String>("t").unwrap().parse::<u64>().unwrap();
-        let interval = matches.get_one::<String>("i").unwrap().parse::<u64>().unwrap();
-        
+        let num_pings = matches
+            .get_one::<String>("n")
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+        let timeout = matches
+            .get_one::<String>("t")
+            .unwrap()
+            .parse::<u64>()
+            .unwrap();
+        let interval = matches
+            .get_one::<String>("i")
+            .unwrap()
+            .parse::<u64>()
+            .unwrap();
+
         assert_eq!(num_pings, 0);
         assert_eq!(timeout, 0);
         assert_eq!(interval, 0);
